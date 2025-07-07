1a3e8cf3c8638a2fb36b6cdd9daba713d384529c
ndmanh2005 <minhanh.hv2512@gmail.com>
2025-07-07 08:24:44 +0700

    Update Warehouse.cs

--- QuanLyKhoHang/Models/Warehouse.cs
+++ QuanLyKhoHang/Models/Warehouse.cs
@@ -6,10 +6,10 @@ namespace QuanLyKhoHang.Models
     public class Warehouse 
     {
         public int Id { get; set; }
-        public string? Code { get; set; } // Mã kho
+        public string? Code { get; set; } 
         public string Name { get; set; }
-        public string? Location { get; set; } // Địa chỉ kho
-        public int IsActive { get; set; } // Trạng thái
+        public string? Location { get; set; } 
+        public int IsActive { get; set; } 
         public bool IsDefault { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
